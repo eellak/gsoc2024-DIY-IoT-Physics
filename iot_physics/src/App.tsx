eb301ab47a1d8c3b98bda48c300374d3b387739f
Manu Dev <manudev0004@gmail.com>
2024-07-05 15:05:39 +0530

    update codes

--- iot_physics/src/App.tsx
+++ iot_physics/src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Signup from './components/Signup';
-import Signin from './components/Signin';
-import AdminPanel from './pages/AdminPanel';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import AdminPanel from "./pages/AdminPanel";
+import Login from "./components/Login";
 
 const App: React.FC = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route path="/signup" component={Signup} />
-                <Route path="/signin" component={Signin} />
-                <Route path="/admin" component={AdminPanel} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <div>
+      <Login/>
+      <Router>
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/signin" component={Signin} />
+          <Route path="/admin" component={AdminPanel} />
+        </Switch>
+      </Router>
+      
+    </div>
+  );
 };
 
 export default App;
