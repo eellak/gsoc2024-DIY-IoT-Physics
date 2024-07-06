fb0d0b061206f6ad61c2f97c328d84d0942f5613
Manu Dev <manudev0004@gmail.com>
2024-07-06 17:49:01 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
 
-const app=express();
+const app = express();
 app.use(cors());
 
-app. listen(5000,()=>console.log("Backend is Running"))
+app.get("/getdata", (req, res) => {
+  res.send("Data from Backend");
+});
+
+app.listen(5000, () => console.log("Backend is Running..."));
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import AdminPanel from "./pages/AdminPanel";
 import Login from "./components/Login";
+import Axios from "axios";
 
 const App: React.FC = () => {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    const response = await Axios.get("http://localhost:5173/getData");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    getData;
+  }, []);
+
   return (
     <div>
-      <Login/>
+      <div>{data}</div>
+      <Login />
       <Router>
         <Switch>
           <Route path="/signup" component={Signup} />
@@ -16,7 +28,6 @@ const App: React.FC = () => {
           <Route path="/admin" component={AdminPanel} />
         </Switch>
       </Router>
-      
     </div>
   );
 };
