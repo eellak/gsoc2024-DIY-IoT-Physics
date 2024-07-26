78d65bc4f6d2643f697a25a795e722e385f5cea0
Manu Dev <manudev0004@gmail.com>
2024-07-27 01:51:56 +0530

    comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+// Imports
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./components/Signup";
